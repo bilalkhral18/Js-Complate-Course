b19accacf25e25c0b64aed2980b118862c935929
bilalkhral18 <tech.com88905@gmail.com>
2025-07-31 18:52:32 +0500

    first commit

---
+++ Lecture-2-Operators-&-Conditions/script.js
@@ -0,0 +1,35 @@
+/*This line print Hello on console window
+console.log("Hello World");*/
+
+// in this topic we have learn how to use arithematic operators
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// let e = a + b + c + d;
+// let f = a - b - c - d;
+// let g = a * b * c * d;
+// let h = a / b / c / d;
+// let i = d % a; // modulus operator
+// let exponentiation = a ** 2; // exponentiation operator
+// console.log(exponentiation); // 100
+// console.log(i);
+// console.log(f);
+// console.log(e);
+// console.log(g);
+// console.log(h);
+
+// Unary Operators
+
+let a = 10;
+let b = 20;
+// post increment : first use the value then increment it
+console.log(a++); // 10
+console.log(a); // 11
+// pre increment : first increment the value then use it
+console.log(++b); // 21
+// post decrement : first use the value then decrement it
+console.log(b--); // 21
+console.log(b); // 20
+// pre decrement : first decrement the value then use it
+console.log(--b); // 19
